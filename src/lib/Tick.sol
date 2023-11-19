ebd39fcd282dd197daf63e90f65c18f3c03cdff6
Seungjin Park <aad336545@gmail.com>
2023-11-20 03:36:01 +0900

    create contructor

---
+++ src/lib/Tick.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+library Tick {
+    function tickSpacingToMaxLiquidityPerTick(int24 tickSpacing) internal pure returns (uint128) {
+        // TODO: write code
+    }
+}
